61db989e3313c43183233305af6de5e87cecb59e
Thomas Ecalle <thomasecalle@gmail.com>
2022-06-03 18:41:11 +0200

    Add Cloud Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'dart:isolate';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:moc_2022/add_user_screen.dart';
 import 'package:moc_2022/analytics_manager.dart';
 import 'package:moc_2022/bo/user.dart';
@@ -43,8 +45,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    _initFirebaseMessaging();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -133,5 +146,100 @@ class Home extends StatelessWidget {
 
   Future<void> _getFakeDataFromNetwork() {
     throw Exception("OUPS, Server Error");
+  }
+
+  void _initFirebaseMessaging() async {
+    final FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    final NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    print('User granted permission: ${settings.authorizationStatus}');
+
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: false,
+    );
+
+    await _initLocalNotifications();
+    _lookForMessagingToken();
+
+    FirebaseMessaging.onMessage.listen((message) => _onMessage(message, onForeground: true));
+    FirebaseMessaging.onMessageOpenedApp.listen(_onNotificationOpenedApp);
+  }
+
+  Future<void> _initLocalNotifications() async {
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: _onNotificationClicked,
+    );
+  }
+
+  void _onNotificationClicked(String? payload) {
+    print("Local notification clicked !");
+  }
+
+  void _onMessage(RemoteMessage message, {bool onForeground = false}) async {
+    final AndroidNotification? android = message.notification?.android;
+    final RemoteNotification? notification = message.notification;
+
+    print("_onMessage: $message");
+    if (notification != null && android != null && onForeground) {
+      const AndroidNotificationChannel channel = AndroidNotificationChannel(
+        'high_importance_channel',
+        'High Importance Notifications',
+        importance: Importance.max,
+      );
+
+      final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+          FlutterLocalNotificationsPlugin();
+
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+
+      flutterLocalNotificationsPlugin.show(
+        notification.hashCode,
+        notification.title,
+        notification.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            icon: android.smallIcon,
+          ),
+        ),
+      );
+    }
+  }
+
+  void _onNotificationOpenedApp(RemoteMessage message) async {
+    print("Une notification a ouvert l'application ! ${message.notification?.title}");
+  }
+
+  void _lookForMessagingToken() async {
+    final String? token = await FirebaseMessaging.instance.getToken();
+    print("Firebase Messaging Token : $token");
+    FirebaseMessaging.instance.onTokenRefresh.listen((String token) {
+      print("Firebase Messaging Token : $token");
+    });
   }
 }
